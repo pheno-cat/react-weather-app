899297edbf6ce2ad9db225375d85b0aebf4a818a
pheno-cat <catherinejalomo@gmail.com>
2021-06-08 16:01:30 -0500

    updated app page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ function App() {
     <div className="App">
       <h1>React Weather App</h1>
       <SearchEngine />
+      <footer>
+        This project was coded by{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.catjalomo.com"
+        >
+          Cat Jalomo
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/pheno-cat/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://peaceful-goldstine-a8794b.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
